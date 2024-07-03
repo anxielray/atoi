81ae9908f59e63566c9d0f2fbb952823b00a3264
anxielray <anxielworld@gmail.com>
2024-07-03 03:02:55 +0300

    added the positive notation

--- main.go
+++ main.go
@@ -23,11 +23,20 @@ func Atoi(arg string) (n int) {
 			digit *= 10
 		}
 		return -n
+	} else if strings.HasPrefix(arg, "+") {
+		arg = arg[1:]
+		for x := len(arg) - 1; x >= 0; x-- {
+			number = int(rune(arg[x]) - 48)
+			n += (digit * number)
+			digit *= 10
+		}
+	} else {
+		for x := len(arg) - 1; x >= 0; x-- {
+			number = int(rune(arg[x]) - 48)
+			n += (digit * number)
+			digit *= 10
+		}
 	}
-	for x := len(arg) - 1; x >= 0; x-- {
-		number = int(rune(arg[x]) - 48)
-		n += (digit * number)
-		digit *= 10
-	}
+
 	return
 }
